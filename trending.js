08a62ffebd1a97baafa42244503def623c7901c2
heartquotelabs-cell <heartquotelabs@gmail.com>
2026-02-11 15:26:12 +0500

    Create trending.js

---
+++ trending.js
@@ -0,0 +1,102 @@
+const quotes = [
+      {
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+      },
+      {
+        text: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+      },
+      {
+        text: "In the middle of every difficulty lies opportunity.",
+        author: "Albert Einstein"
+      },
+      {
+        text: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+      },
+      {
+        text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill"
+      },
+      {
+        text: "You miss 100% of the shots you don't take.",
+        author: "Wayne Gretzky"
+      },
+      {
+        text: "The only limit to our realization of tomorrow is our doubts of today.",
+        author: "Franklin D. Roosevelt"
+      },
+      {
+        text: "Do not watch the clock. Do what it does. Keep going.",
+        author: "Sam Levenson"
+      },
+      {
+        text: "The best way to predict the future is to invent it.",
+        author: "Alan Kay"
+      },
+      {
+        text: "Life is 10% what happens to us and 90% how we react to it.",
+        author: "Charles R. Swindoll"
+      },
+      {
+        text: "Whether you think you can or you think you can't, you're right.",
+        author: "Henry Ford"
+      },
+      {
+        text: "The only person you are destined to become is the person you decide to be.",
+        author: "Ralph Waldo Emerson"
+      },
+      {
+        text: "I have not failed. I've just found 10,000 ways that won't work.",
+        author: "Thomas Edison"
+      },
+      {
+        text: "It does not matter how slowly you go as long as you do not stop.",
+        author: "Confucius"
+      },
+      {
+        text: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+        author: "Thomas Edison"
+      },
+      {
+        text: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt"
+      },
+      {
+        text: "The only true wisdom is in knowing you know nothing.",
+        author: "Socrates"
+      },
+      {
+        text: "Strive not to be a success, but rather to be of value.",
+        author: "Albert Einstein"
+      },
+      {
+        text: "You must be the change you wish to see in the world.",
+        author: "Mahatma Gandhi"
+      },
+      {
+        text: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+        author: "Zig Ziglar"
+      },
+      {
+        text: "The purpose of our lives is to be happy.",
+        author: "Dalai Lama"
+      },
+      {
+        text: "Get busy living or get busy dying.",
+        author: "Stephen King"
+      },
+      {
+        text: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+        author: "Brian Tracy"
+      },
+      {
+        text: "The mind is everything. What you think you become.",
+        author: "Buddha"
+      },
+      {
+        text: "We become what we think about.",
+        author: "Earl Nightingale"
+      }
+    ];
